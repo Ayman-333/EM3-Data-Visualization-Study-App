2731f21209408b8d1f537c362c02d7e9f3c84695
a@fkrtech.com <a@fkrtech.com>
2019-11-03 13:54:31 +0300

    Replace plot with stacked bar chart.

--- EM3/src/components/App.js
+++ EM3/src/components/App.js
@@ -6,6 +6,7 @@
  * @flow
  */
 import LineChartPlot from './LineChartPlot';
+import { StackedBarChart } from 'react-native-svg-charts';
 import Questionnaire from './Questionnaire';
 import SurveyHeader from './SurveyHeader';
 
@@ -21,13 +22,57 @@ import {
 
 class App extends React.Component {
   render() {
+
+    const data = [
+      {
+          month: new Date(2015, 0, 1),
+          apples: 3840,
+          bananas: 1920,
+          cherries: 960,
+          dates: 400,
+          oranges: 400,
+      },
+      {
+          month: new Date(2015, 1, 1),
+          apples: 1600,
+          bananas: 1440,
+          cherries: 960,
+          dates: 400,
+      },
+      {
+          month: new Date(2015, 2, 1),
+          apples: 640,
+          bananas: 960,
+          cherries: 3640,
+          dates: 400,
+      },
+      {
+          month: new Date(2015, 3, 1),
+          apples: 3320,
+          bananas: 480,
+          cherries: 640,
+          dates: 400,
+      },
+    ]
+    
+    const colors = ['#7b4173', '#a55194', '#ce6dbd', '#de9ed6']
+    const keys = ['apples', 'bananas', 'cherries', 'dates']
+    
+
     return (
       <>
         <SurveyHeader style={styles.header}/>
         <SafeAreaView style={styles.container}>
           <View>
-            <View style={styles.plotBody}>
-              <LineChartPlot />
+            <View >
+              <StackedBarChart
+                style={{ height: 200 }}
+                keys={keys}
+                colors={colors}
+                data={data}
+                showGrid={false}
+                contentInset={{ top: 30, bottom: 30 }}
+            />
             </View>
             <View style={styles.container}>
               <Questionnaire />
