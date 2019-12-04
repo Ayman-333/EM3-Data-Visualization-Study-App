6022f4bdd43bc066c59834f727e01aaf39439942
a@fkrtech.com <a@fkrtech.com>
2019-12-04 12:48:43 +0300

    Experiments with D3

--- EM3/src/components/App.js
+++ EM3/src/components/App.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 import {View, StyleSheet, SafeAreaView} from 'react-native';
 import CustomStackedBarChart from './CustomStackedBarChart';
 import Questionnaire from './Questionnaire';
+import Sprial from './Spiral';
 import SurveyHeader from './SurveyHeader';
 import LineChartPlot from './LineChartPlot';
+import BarChart from './BarChart';
 
 export default class App extends Component {
 
+ 
+
   render() {
+    const data = [
+      { label: 'Jan', value: 500 },
+      { label: 'Feb', value: 312 },
+      { label: 'Mar', value: 424 },
+      { label: 'Apr', value: 745 },
+      { label: 'May', value: 89 },
+      { label: 'Jun', value: 434 },
+      { label: 'Jul', value: 650 },
+      { label: 'Aug', value: 980 },
+      { label: 'Sep', value: 123 },
+      { label: 'Oct', value: 186 },
+      { label: 'Nov', value: 689 },
+      { label: 'Dec', value: 643 }
+    ]
     return (
       <>
         <SurveyHeader style={styles.header}/>
@@ -17,7 +35,7 @@ export default class App extends Component {
             <CustomStackedBarChart />
           </View>
           <View style={styles.container}>
-            <Questionnaire />
+            <BarChart data={data} round={100} unit="€"/>
           </View>
         </SafeAreaView>
       </>
