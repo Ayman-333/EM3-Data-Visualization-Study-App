1630d899f0ad1c341681502ea82b71d785c61e26
Ayman-aa1405810 <aa1405810@qu.edu.qa>
2019-12-16 12:20:51 +0300

    Heatmap added

--- EM3/src/components/App.js
+++ EM3/src/components/App.js
@@ -15,30 +15,16 @@ export default class App extends Component {
 
 
   render() {
-    const data = [
-      { label: 'Jan', value: 500 },
-      { label: 'Feb', value: 312 },
-      { label: 'Mar', value: 424 },
-      { label: 'Apr', value: 745 },
-      { label: 'May', value: 89 },
-      { label: 'Jun', value: 434 },
-      { label: 'Jul', value: 650 },
-      { label: 'Aug', value: 980 },
-      { label: 'Sep', value: 123 },
-      { label: 'Oct', value: 186 },
-      { label: 'Nov', value: 689 },
-      { label: 'Dec', value: 643 },
-    ]
     return (
       <>
         <SurveyHeader style={styles.header}/>
         <SafeAreaView style={styles.container}>
           <View  style={styles.plotBody}>
-            <CustomStackedBarChart />
-          </View>
-          <View style={styles.container}>
             <Heatmap />
           </View>
+          {/* <View style={styles.container}>
+            <Questionnaire />
+          </View> */}
         </SafeAreaView>
       </>
     );
