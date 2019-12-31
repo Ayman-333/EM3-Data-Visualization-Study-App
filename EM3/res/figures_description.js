f4d009b5476b9243257a643348d0e06398adf919
Ayman-aa1405810 <aa1405810@qu.edu.qa>
2019-12-31 14:55:22 +0300

    labels adjusted for conventional plots

--- EM3/res/figures_description.js
+++ EM3/res/figures_description.js
@@ -1,9 +1,9 @@
 module.exports = {
   descriptions: {
-    heatmap: 'Shows daily energy consumption (in kWh) for all the rooms in the house. Pressing any colored period would show more information. Green spots means energy efficient while red means excessive.',
+    heatmap: 'Shows daily energy consumption (in Wh) for all the rooms in the house. Pressing any colored period would show more information. Green spots means energy efficient while red means excessive.',
     spiral: 'Shows the total daily energy consumption (in kWh) for the whole year for a house. Pressing on a bar shows a stooltip with more information.',
     stackedBar: 'Shows energy consumption (in kWh) for each appliance (and their different brands) throughout the day. An appliance can be chosen from the scrollable list',
-    stackedArea: 'Shows energy consumption (in kWh) for multiple hours throughtout the day.',
+    stackedArea: 'Shows energy consumption (in kWh) for multiple hours throughout the day.',
     bar: 'Shows energy consumption (in kWh) for the whole month as bars.',
     line: 'Shows energy consumption (in kWh) for the whole month as a continuous line.',
   },
