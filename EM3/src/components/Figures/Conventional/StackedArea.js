28fc35888294b8f9615e0138a80eb56422b2db78
Ayman-aa1405810 <aa1405810@qu.edu.qa>
2020-01-16 22:12:58 +0300

    fixed stacked area chart on small screens, added scrollview

--- EM3/src/components/Figures/Conventional/StackedArea.js
+++ EM3/src/components/Figures/Conventional/StackedArea.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { Svg, G, Text } from 'react-native-svg';
 import { StackedAreaChart, YAxis, XAxis } from 'react-native-svg-charts';
 import * as shape from 'd3-shape';
@@ -23,27 +23,30 @@ class StackedArea extends Component {
     return (
       <>
         <View style={{ flexDirection: 'row' }}>
-          <Svg
-            key={'ylabelTitle'}
-            height={300}
-            width={25}>
-            <G y={200} rotation={-90}>
-              <Text
-                fill="black"
-                stroke="black"
-                fontSize="15"
-                fontWeight="lighter"
-                x={10}
-                y={22}
-                textAnchor="start">
-                {"Power (W)"}
-              </Text>
-            </G>
-          </Svg>
-          <View style={styles.container}>
+          <ScrollView
+            contentContainerStyle={styles.container}
+            horizontal={true}>
+            <Svg
+              key={'ylabelTitle'}
+              height={300}
+              width={25}
+              marginRight={18}>
+              <G y={200} rotation={-90}>
+                <Text
+                  fill="black"
+                  stroke="black"
+                  fontSize="15"
+                  fontWeight="lighter"
+                  x={10}
+                  y={13}
+                  textAnchor="start">
+                  {"Power (W)"}
+                </Text>
+              </G>
+            </Svg>
             <StackedAreaChart
-              style={{ flex: 1 }}
-              contentInset={{ top: 10, bottom: 10, left: 25 }}
+              style={{ width: 400 }}
+              contentInset={{ top: 10, bottom: 10, left: 0 }}
               spacingInner={spacingInner}
               spacingOuter={spacingOuter}
               data={data.expenseData}
@@ -53,7 +56,7 @@ class StackedArea extends Component {
               {/* <Grid /> */}
             </StackedAreaChart>
             <YAxis
-              style={{ position: 'absolute', top: 0, bottom: 0 }}
+              style={{ position: 'absolute', top: 0, bottom: 0, left: 25 }}
               data={StackedAreaChart.extractDataPoints(data.expenseData, data.keys)}
               contentInset={{ top: 10, bottom: 25 }}
               svg={{
@@ -66,7 +69,7 @@ class StackedArea extends Component {
               }}
             />
             <XAxis
-              style={{ height: 100, position: "absolute", bottom: -80, width: 430 }}
+              style={{ height: 100, position: "absolute", bottom: -80, left: -5, width: 550 }}
               data={data.expenseData}
               formatLabel={(value, index) =>
                 data.expenseData[index].date.format('HH:mm')
@@ -95,8 +98,9 @@ class StackedArea extends Component {
               fill: 'black',
             }}
           /> */}
-            <Legends data={data} />
-          </View>
+          </ScrollView>
+          <Legends data={data} />
+
         </View>
         {/* XAxis Label */}
         <Svg
@@ -108,7 +112,7 @@ class StackedArea extends Component {
             stroke="black"
             fontSize="15"
             fontWeight="lighter"
-            x={200}
+            x={Dimensions.get('window').width / 2}
             y={15}
             textAnchor="middle">
             {data.expenseData[0].date.format('MMM Do YYYY')}
@@ -121,15 +125,14 @@ class StackedArea extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     textAlign: 'center',
     marginTop: 15,
     marginRight: 20,
     paddingBottom: 20,
-    marginHorizontal: 15,
+    marginHorizontal: 5,
     flexDirection: "row",
     height: 300,
-    width: this.screenWidth,
+    width: 480,
   },
 });
 
